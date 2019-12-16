cc298667c969e72d25ce8e118bec424defebbf7c
jackee67rus <jackee67rus@mail.ru>
2019-12-16 22:58:16 +0300

    добавил новую ветку

---
+++ 2222.py
@@ -0,0 +1,11 @@
+a = int(input())
+s = a
+sum = []
+while s!= 0:
+    a = int(input())
+    s = s + a
+for i in range(len(a)-1):
+    sum + a.append[i] ** 2
+if s == 0:
+    print('noob')
+else: print(sum)
